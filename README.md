bd81edec5a6309e32ce319e8fcda7a62eb047ba2
Nowell Strite <nowell@strite.org>
2013-07-30 13:38:47 -0700

    Initial commit of the Chiropractor codebase.
    Change-Id: I0191b02a3667116582c9253de32c2e157b3c4c6f

---
+++ README.md
@@ -0,0 +1,86 @@
+Chiropractor
+============
+
+Straighten out your Backbone.js
+
+Usage
+-----
+
+For the time being we are not bundling all of the dependencies for this module
+into the `chiropractor.js` file, instead you will need to define those
+dependencies yourself. You can reference the `bower.json` file to get a list
+of all of the dependencies (both development and production) or reference
+`build.js` to see what the bare minimum requirejs configuration file looks like
+to support this module.
+
+```
+require.config({
+    paths: {
+        'chiropractor':       'bower_components/chiropractor/chiropractor',
+        'handlebars':         'bower_components/require-handlebars-plugin/Handlebars',
+        'json3':              'bower_components/json3/lib/json3',
+        'underscore':         'bower_components/underscore/underscore',
+        'backbone':           'bower_components/backbone/backbone',
+        'jquery':             'bower_components/jquery/jquery',
+        'jquery.cookie':      'bower_components/jquery.cookie/jquery.cookie',
+        'backbone.subroute':  'bower_components/backbone.subroute/backbone.subroute'
+    },
+
+    hbs: {
+        disableI18n: true,
+        disableHelpers: true
+    },
+
+    pragmasOnSave: {
+        excludeHbsParser : true,
+        excludeHbs: true,
+        excludeAfterBuild: true
+    },
+
+    shim: {
+        'backbone': {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        },
+        'jquery.cookie': {
+            deps: ['jquery'],
+            exports: 'jQuery.cookie'
+        },
+        json3: {
+            exports: 'JSON'
+        }
+    }
+});
+```
+
+Building
+--------
+
+To get started developing Chiropractor you must first run two commands to
+install all of the dependencies:
+
+    npm install
+    bower install
+
+In order to compile the Chiropractor code for release you must run:
+
+	grunt
+
+Which will generate `chiropractor.js` as well as `chiropractor.min.js` in the
+root of the project. Whenever you wish to run this command, please be sure to
+increment the version defined in `package.json` as well as `bower.json`.
+
+Tests
+-----
+
+There are two ways to run tests:
+
+	npm test
+
+Or if you want more control over how your tests are run (such as continuous
+testing with auto-running tests on file changes you can run:
+
+	./node_modules/.bin/karma start --dev --browsers Chrome,PhantonJS
---
+++ specs/karma.js
@@ -0,0 +1,81 @@
+require.config({
+    packages: [
+        {name: "chiropractor", location: "src"}
+    ],
+
+    baseUrl: '/base',
+
+    hbs: {
+        disableI18n: true,
+        disableHelpers: true
+    },
+
+    paths: {
+        // Application Dependencies
+        'handlebars':         'bower_components/require-handlebars-plugin/Handlebars',
+        'json3':              'bower_components/json3/lib/json3',
+        'underscore':         'bower_components/underscore/underscore',
+        'backbone':           'bower_components/backbone/backbone',
+        'jquery':             'bower_components/jquery/jquery',
+        'jquery.cookie':      'bower_components/jquery.cookie/jquery.cookie',
+        'backbone.subroute':  'bower_components/backbone.subroute/backbone.subroute',
+        // Testing Dependencies
+        'sinon':              'bower_components/sinonjs/sinon',
+        'es5-shim':           'bower_components/es5-shim/es5-shim',
+        'chai':               'bower_components/chai/chai',
+        'expectjs':           'bower_components/expect/expect',
+        'expect':             'lib/expect',
+        'mocha':              'lib/mocha',
+        'browser':            'lib/browser'
+    },
+
+    pragmasOnSave: {
+        excludeHbsParser : true,
+        excludeHbs: true,
+        excludeAfterBuild: true
+    },
+
+    // Shims are used to set dependencies for third-party modules which
+    // do not require their dependencies. first-party modules and forks
+    // should not require entries in shims, as they should be able to
+    // use define() or require() as appropriate to ensure all their
+    // dependencies are available
+    shim: {
+        // Application Shims
+        'backbone': {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        },
+        'jquery.cookie': {
+            deps: ['jquery'],
+            exports: 'jQuery.cookie'
+        },
+        // Testing Shims
+        json3: {
+            exports: 'JSON'
+        },
+        expectjs: {
+            exports: 'expect'
+        },
+        'sinon': {
+            exports: 'sinon',
+        },
+        chai: {
+            deps: ['es5-shim']
+        }
+    },
+    enforceDefine: true
+});
+
+require([
+    'require',
+    'specs/setup/mocha',
+    'specs/main'
+], function(require, mochaSetup, testSuite) {
+    mochaSetup();
+    testSuite();
+    window.__karma__.start();
+});
