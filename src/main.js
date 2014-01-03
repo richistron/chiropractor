98dd4c9323df9f144b168a74ea15f850fd5a8521
incognos <sina@khelil.com>
2014-01-03 10:15:06 -0500

    Pub Sub Broadcasting for Nagini Crete, fixes #21
    Change-Id: I468f9e6952479fc947b44c787ceedc34d2f40783

--- src/main.js
+++ src/main.js
@@ -8,12 +8,34 @@ define(function(require) {
         Views = require('./views'),
         Models = require('./models'),
         Collections = require('./collections'),
-        Routers = require('./routers');
+        Routers = require('./routers'),
+        Channels = {},
+        Subscribe,
+        Publish;
 
     require('./debug');
     require('./hbs');
 
+    Subscribe = function(channel,object) {
+      if (!Channels[channel]) {
+        Channels[channel] = [];
+        Channels[channel].push(object);
+      }
+    };
+    Publish = function(channel,options) {
+      if (Channels[channel]) {
+        var listeners = Channels[channel];
+        _.each(listeners, function(listener){
+          listener.trigger(channel,options);
+        });
+      }
+    };
+
     return {
+        // Channel events
+        Subscribe: Subscribe,
+        Publish: Publish,
+        Channels: Channels,
         Collection: Collections.Base,
         Collections: Collections,
         Events: Backbone.Events,
